b6642dfcdbf34faa3dc973263fa28288bdc7955c
Tim Batty <tbatty7@gmail.com>
2022-06-23 21:51:08 -0500

    added button to navigate to slice kata screen

--- src/LandingScreen.tsx
+++ src/LandingScreen.tsx
@@ -7,9 +7,14 @@ export function LandingScreen(props: {navigation: any}) {
     return props.navigation.navigate('Help');
   }
 
+  function handleSliceKataButton() {
+    return props.navigation.navigate('SliceKata');
+  }
+
   return (
     <View>
       <Button title="React Help" onPress={handleHelpButton} />
+      <Button title="Redux Slice Kata" onPress={handleSliceKataButton} />
     </View>
   );
 }
--- src/__tests__/LandingScreen.tests.tsx
+++ src/__tests__/LandingScreen.tests.tsx
@@ -5,14 +5,23 @@ import {fireEvent, render, RenderAPI} from '@testing-library/react-native';
 
 describe('LandingScreen', () => {
   const navigate = jest.fn();
+  let landingScreen;
+  beforeEach(() => {
+    landingScreen = render(<LandingScreen navigation={{navigate}} />);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('should render', () => {
-    const landingScreen = render(<LandingScreen navigation={{navigate}} />);
     landingScreen.getByText('React Help');
   });
   it('should navigate to help screen when button pushed', () => {
-    const landingScreen = render(<LandingScreen navigation={{navigate}} />);
     pressButtonWithText(landingScreen, 'React Help');
     expect(navigate).toHaveBeenCalledWith('Help');
+  });
+  it('should navigate to redux slice kata when button pushed', () => {
+    pressButtonWithText(landingScreen, 'Redux Slice Kata');
+    expect(navigate).toHaveBeenCalledWith('SliceKata');
   });
 });
 
