00d0aabe27315f0c8ea39bd1358774c39da910e3
Tim Batty <tbatty7@gmail.com>
2022-06-24 23:02:57 -0500

    Added render test for SliceKataScreen

---
+++ src/__tests__/SliceKataScreen.tests.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SliceKataScreen} from '../SliceKataScreen';
+import {render} from '@testing-library/react-native';
+
+describe('SliceKataScreen', () => {
+  it('renders', () => {
+    const sliceScreen = render(<SliceKataScreen />);
+    sliceScreen.getByPlaceholderText('Ticket Price');
+    sliceScreen.getByText('Submit');
+  });
+});
