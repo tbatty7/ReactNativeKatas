875c55a592c9ab0d03cb80b959a70025cf00f36d
Tim Batty <tbatty7@gmail.com>
2022-06-24 22:12:26 -0500

    fixed ticketSlice test

--- src/__tests__/TicketSlice.tests.ts
+++ src/__tests__/TicketSlice.tests.ts
@@ -2,8 +2,7 @@ import {createTicket, initialState, ticketSlice} from '../TicketSlice';
 
 describe('TicketSlice', () => {
   it('adds ticket total to ticket', () => {
-    const input = {totalPrice: '12.00'};
-    const newState = ticketSlice.reducer(initialState, createTicket(input));
-    expect(newState).toEqual({totalPrice: {totalPrice: '12.00'}});
+    const newState = ticketSlice.reducer(initialState, createTicket('12.00'));
+    expect(newState).toEqual({totalPrice: '12.00'});
   });
 });
