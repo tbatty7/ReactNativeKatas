7c3f06f1e049bcee187cce94a145439c74c3e454
Tim Batty <tbatty7@gmail.com>
2022-08-27 18:02:33 -0400

    add basic animation to ball

--- src/SimpleAnimationsScreen.tsx
+++ src/SimpleAnimationsScreen.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {useEffect, useState} from 'react';
 
 export function SimpleAnimationsScreen() {
+  const [translation, setTranslation] = useState(0);
+
+  useEffect(() => {
+    for (let i = 0; i < 1000; i++) {
+      setTimeout(() => {
+        setTranslation(i);
+      }, 25 * i);
+    }
+  }, []);
+
   return (
     <View style={styles.screen}>
-      <View accessibilityLabel={'ball'} style={styles.ball}></View>
+      <View
+        accessibilityLabel={'ball'}
+        style={[styles.ball, {transform: [{translateX: translation}]}]}></View>
     </View>
   );
 }
